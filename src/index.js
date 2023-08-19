b076ac73bca373d89fce1f6750911326be6a258a
Vladimir <vokar90@yandex.ru>
2023-08-19 19:50:01 +0300

    fix: indent

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ module.exports = function check(str, bracketsConfig) {
   str = str.split('');
 
   for(let i = 0; i < str.length; i++) {
-      for (let j = 0; j < bracketsConfig.length; j++) {
-        if (str.length === 0) {
-          break
-        }
-
-          if (str[i] === (bracketsConfig[j])[0] && 
-          str[i+1] === (bracketsConfig[j])[1]) {
-
-              str.splice(i, 2)
-              i = -1;
-          }
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str.length === 0) {
+        break
       }
+
+      if (str[i] === (bracketsConfig[j])[0] && 
+      str[i+1] === (bracketsConfig[j])[1]) {
+
+        str.splice(i, 2)
+        i = -1;
+      }
+    }
   }
 
   return Boolean(!str.length)
