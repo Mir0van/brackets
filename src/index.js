0e93c38d641c3879f619d70ce3b2dbf934381bb9
Vladimir <vokar90@yandex.ru>
2023-08-19 19:47:41 +0300

    feat: function improvement

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ module.exports = function check(str, bracketsConfig) {
 
   for(let i = 0; i < str.length; i++) {
       for (let j = 0; j < bracketsConfig.length; j++) {
+        if (str.length === 0) {
+          break
+        }
 
           if (str[i] === (bracketsConfig[j])[0] && 
           str[i+1] === (bracketsConfig[j])[1]) {
