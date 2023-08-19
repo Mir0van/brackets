cadfa8737f4f48ef1f365e74f36df833ffd94088
Vladimir <vokar90@yandex.ru>
2023-08-19 19:33:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) {  
+  str = str.split('');
+
+  for(let i = 0; i < str.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+
+          if (str[i] === (bracketsConfig[j])[0] && 
+          str[i+1] === (bracketsConfig[j])[1]) {
+
+              str.splice(i, 2)
+              i = -1;
+          }
+      }
+  }
+
+  return Boolean(!str.length)
 }
